d2ad67eecdc6eace85b4124ffcdafc86509c802e
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-15 22:00:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,12 +29,6 @@ class Song
   end 
   
   def self.genre_count
-    #return a hash 
-    #key names are genres 
-    #values are numbers of songs
-    #check if hash contains key of particular genre
-    #increment value by one if so
-    #create new key/value pair if not
    genre_hash = {}
    @@genres.each do |names| 
    if genre_hash.include?(names) 
@@ -47,6 +41,14 @@ class Song
   end
   
   def self.artist_count
-    
+       artist_hash = {}
+   @@artists.each do |names| 
+   if artist_hash.include?(names) 
+      artist_hash[names] += 1
+    else 
+      artist_hash[names] = 1 
+   end 
+   end 
+   artist_hash
   end 
 end 
